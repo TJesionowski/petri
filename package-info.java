da066a981a428a3f68a4f34c5376827a8946d000
Timothy Jesionowski <timothynjesionowski@gmail.com>
2018-01-25 19:51:53 -0500

    Put in link in package-info related to save/reload feauture
    The link points out what appears to be a simple solution, however further research is needed to determine if it will properly recurse through subclasses.

--- package-info.java
+++ package-info.java
@@ -1,5 +1,7 @@
 /**
  * 
+ * TODO Main TODO list
+ * TODO Implement save/load as per https://stackoverflow.com/questions/26251006/best-way-to-implement-save-and-restore-object-attribute-state#26251321
  */
 /**
  * @author tim
