ee552f68fc05630029fb84b38ce30cf60d39d50b
Timothy Jesionowski <timothynjesionowski@gmail.com>
2018-01-25 19:21:05 -0500

    Fleshed out the Position subclass
    May need to break Position out into its own full class to impllement XML saving. not terribly sure though.

--- Cell.java
+++ Cell.java
@@ -2,6 +2,32 @@ package petri;
 
 public class Cell {
 	private class Position {
-		
+		// This subclass is meant to contain the position vector and related methods
+		private double x, y;
+		public Position() {
+			x = 0;
+			y = 0;
+		}
+		public Position(double a, double b) {
+			x = a;
+			y = b;
+		}
+		public double dist(Position other) {
+			return Math.sqrt(Math.abs(Math.pow( (x - other.getX()), 2) + Math.pow( (y - other.getY()), 2)));
+		}
+		public double getX() {
+			return x;
+		}
+		public double getY() {
+			return y;
+		}
+		// test method
 	}
+	
+	public float calcLight(Position pos) {
+		//this is where the amount of light is calculated
+		// TODO
+	}
+	// TODO constructor
+	// TODO test method
 }
